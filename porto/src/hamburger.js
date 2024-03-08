65d3eea1c418c1e12a51cc184570d9d3fe498890
Joey Shiundu Wesonga <shiundujoey@gmail.com>
2024-03-08 23:55:29 +0300

    Update slideshow.js with improved styling and font

---
+++ porto/src/hamburger.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const HamburgerMenu = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className={`hamburger ${isActive ? 'is-active' : ''}`} onClick={toggleMenu}>
+      <div className="hamburger__container">
+        <div className="hamburger__inner"></div>
+        <div className="hamburger__hidden"></div>
+      </div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- porto/src/intro.js
+++ porto/src/intro.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import 'devicon/devicon.min.css';
+import HamburgerMenu from './hamburger';
 
 function Intro() {
     return (
         <div className='container w-1/2 space-y-10 px-auto text-5xl ml-auto pb-9 '>
+            <HamburgerMenu  />
             <p className='px-10 '>Hi,I am Joey Shiundu<br/>
             A Frontend Developer from Nairobi.</p>
             <button className='bg-black text-white p-4 rounded-2xl'>Know More</button>
