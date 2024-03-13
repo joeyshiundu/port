bb6b3bb7317c65fb6628466ebdee587237156b52
Joey Shiundu Wesonga <shiundujoey@gmail.com>
2024-03-13 09:33:02 +0300

    Update App.css and footer.js

--- porto/src/footer.js
+++ porto/src/footer.js
@@ -8,9 +8,10 @@ const Footer = () => {
     <footer className="bg-gray-800 p-20">
       <div className="container mx-auto">
         <div className="flex justify-center">
-          <div className="w-1/4">
-            <h5 className="text-2xl font-bold italic text-white">Carpe Diem</h5>
-            <p className="text-white mt-4 italic">Carpe Momento</p>
+          <div className="w-1/2">
+            <h4 className="text-2xl font-bold font-mono text-cyan-500/70">Joey</h4>
+            <h5 className="text-2xl font-bold text-white font-sans">Shiundu</h5>
+            <p className="text-white text-sm italic pt-4">"Where creativity meets Innovation"</p>
           </div>
         </div>
       </div>
