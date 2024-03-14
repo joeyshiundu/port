a5599ebee6cb480ab07338221705afd2b3b779f3
Joey Shiundu Wesonga <shiundujoey@gmail.com>
2024-03-14 19:21:14 +0300

    Refactor slideshow.js: Update text and formatting

--- porto/src/slideshow.js
+++ porto/src/slideshow.js
@@ -9,7 +9,7 @@ function Slideshow() {
         
         <div className="flex gap-x-4 max-md:flex-col max-md:m-auto ">
         <div className='  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-600/30 duration-400 ...  container space-y-10 p-auto text-5xl shadow-xl  shadow-blue-600/50 font-mono max-md:text-3xl ' >Web Development
-            <p className="text-sm font-sans  ">
+            <p className="text-sm font-sans p-6 ">
               Experienced web developer proficient in React, HTML, CSS, JavaScript, and Tailwind CSS. Skilled in crafting dynamic and responsive interfaces using React, with a keen eye for visually appealing layouts through HTML and CSS. Efficiently utilizes Tailwind CSS for streamlined styling, contributing to polished and user-friendly digital experiences.</p>
         </div>
         <div className=' container  px-auto text-5xl shadow-xl shadow-blue-600/50 font-mono transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 max-md:text-3xl hover:bg-blue-500/50 duration-500 ...'>UI/UX Design
@@ -17,7 +17,7 @@ function Slideshow() {
         </div>
         <div className=' container px-auto text-5xl shadow-xl font-mono shadow-blue-600/50 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 max-md:text-3xl hover:bg-blue-500/50 duration-500 ... '>Cloud Computing
             <p className="text-sm font-sans">
-        Proficient in cloud computing, I specialize in deploying and managing scalable applications on cloud platforms. I am skilled in utilizing cloud services, such as AWS, to optimize application performance and reliability. My expertise extends to cloud security, ensuring robust and secure cloud infrastructure. I am also adept in cloud automation, streamlining cloud operations for efficient and cost-effective cloud solutions.Currently, I am working on AWS Certified Associate Certificate
+            Proficient in cloud computing, specializing in deploying and managing scalable applications on AWS. Skilled in optimizing performance and reliability, ensuring robust security, and streamlining operations for cost-effective solutions. Currently pursuing AWS Certified Associate Certificate.
         </p>
         </div>
 
